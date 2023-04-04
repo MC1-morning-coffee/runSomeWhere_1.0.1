27543d3ae38dc7162445e073693de1f959e53e3f
yuncoffee <dong072815@gmail.com>
2023-04-04 09:04:32 +0900

    Initial Commit

---
+++ runSomeWhere_1.0.1/runSomeWhere_1_0_1App.swift
@@ -0,0 +1,17 @@
+//
+//  runSomeWhere_1_0_1App.swift
+//  runSomeWhere_1.0.1
+//
+//  Created by Yun Dongbeom on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct runSomeWhere_1_0_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
