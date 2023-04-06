8f42dc9fbf74d5903137a4c0fc1be3ead05347e3
LIM-YUSANG <119108929+LIM-YUSANG@users.noreply.github.com>
2023-04-06 19:54:43 +0900

    Update SoundHandler.swift

--- runSomeWhere_1.0.1/Handlers/SoundHandlers/SoundHandler.swift
+++ runSomeWhere_1.0.1/Handlers/SoundHandlers/SoundHandler.swift
@@ -10,19 +10,19 @@ import AVFoundation
 import Combine
 
 
-@State private var currentCount = 0
-@State private var backgroundMusicPlayer: AVPlayer?
-@State private var soundEffectPlayer: AVPlayer?
-@State private var currentPlayCount = 0
-
-let musicFileName = backgroundMusicFiles[0] // count starts at 1, but array index starts at 0
-let musicFileUrl = Bundle.main.url(forResource: musicFileName, withExtension: musicFileExtension)!
-backgroundMusicPlayer = AVPlayer(url: musicFileUrl)
-backgroundMusicPlayer?.volume = volume
-backgroundMusicPlayer?.play()
-
-let effectFileName = soundEffectFiles[0] // count starts at 1, but array index starts at 0
-let effectFileUrl = Bundle.main.url(forResource: effectFileName, withExtension: musicFileExtension1)!
-soundEffectPlayer = AVPlayer(url: effectFileUrl)
-soundEffectPlayer?.volume = volume1
-soundEffectPlayer?.play()
+//@State private var currentCount = 0
+//@State private var backgroundMusicPlayer: AVPlayer?
+//@State private var soundEffectPlayer: AVPlayer?
+//@State private var currentPlayCount = 0
+//
+//let musicFileName = backgroundMusicFiles[0] // count starts at 1, but array index starts at 0
+//let musicFileUrl = Bundle.main.url(forResource: musicFileName, withExtension: musicFileExtension)!
+//backgroundMusicPlayer = AVPlayer(url: musicFileUrl)
+//backgroundMusicPlayer?.volume = volume
+//backgroundMusicPlayer?.play()
+//
+//let effectFileName = soundEffectFiles[0] // count starts at 1, but array index starts at 0
+//let effectFileUrl = Bundle.main.url(forResource: effectFileName, withExtension: musicFileExtension1)!
+//soundEffectPlayer = AVPlayer(url: effectFileUrl)
+//soundEffectPlayer?.volume = volume1
+//soundEffectPlayer?.play()
